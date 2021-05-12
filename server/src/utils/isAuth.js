8d865abcae923d82b410eb8dcb7c061ff9dfebff
Alen Zeinolov <github@alenzeinolov.com>
2021-05-12 21:21:03 +0600

    Add isAuth middleware

--- server/src/utils/isAuth.js
+++ server/src/utils/isAuth.js
@@ -3,12 +3,12 @@ import APIError from "../utils/APIError";
 const authKey = "authorization";
 const tokenPrefix = "Bearer ";
 const isAuth = (req, _, next) => {
-  // if (authKey in req.headers && req.headers[authKey].includes(tokenPrefix)) {
-  // const authStr = req.headers[authKey];
-  // const token = authStr.split(" ")[1];
-  // console.log(token);
-  // next();
-  // }
+  if (authKey in req.headers && req.headers[authKey].includes(tokenPrefix)) {
+    const authStr = req.headers[authKey];
+    const token = authStr.split(" ")[1];
+    console.log(token);
+    next();
+  }
   next(new APIError("You are not authenticated.", "fail", 401));
 };
 
